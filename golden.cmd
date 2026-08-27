9a265a745a752f3c44ea144efe287396dbc5a0c2
Yuan Yuhao <325063151@qq.com>
2026-08-27 16:56:27 -0400

    add golden cmd

---
+++ golden.cmd
@@ -0,0 +1,12 @@
+:: ItemViewer development command reference
+:: Copy one command at a time and paste it into a terminal at the repository root.
+
+:: Start the Demo page
+npm start
+
+:: Demo URL
+:: http://localhost:4200/
+
+:: Install dependencies
+npm install
+
